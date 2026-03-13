6a34b5b7c8a1245970452f3b8bddae67a8f8484e
More Srinivas <moresrinivas2251@gmail.com>
2026-03-13 18:24:57 +0530

    Initial Commit

---
+++ Week5/DigitalBookStore/Service-registry/src/test/java/com/lpu/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpu.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
